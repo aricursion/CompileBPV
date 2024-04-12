dd8ae928e2185612540d28459da8ecf87f2aefd0
zaxioms <zbattleman@gmail.com>
2024-04-12 14:40:16 -0400

    added of string for variable

--- lib/variable.ml
+++ lib/variable.ml
@@ -3,3 +3,5 @@ type t = string
 let ctr = ref 0
 
 let new_var = fun _ -> ctr := !ctr + 1 ; string_of_int !ctr
+
+let of_string = fun s -> s
--- lib/variable.mli
+++ lib/variable.mli
@@ -1,3 +1,5 @@
 type t
 
 val new_var : unit -> t
+
+val of_string : string -> t
