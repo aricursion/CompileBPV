5ddfa9ee3b0f7138d0027565253002d1c7f86809
sonyasimk <sonya.simkin@gmail.com>
2024-04-13 13:33:36 -0400

    appease dune build

--- lib/ASTs/ast.ml
+++ lib/ASTs/ast.ml
@@ -17,11 +17,11 @@ type term =
 let rec pp_term e = 
   match e with 
   | Var x -> Variable.pp_var x
-  | Lambda (x, t, e) -> "\\" ^  Variable.pp_var x ^ "." ^ pp_term e
+  | Lambda (x, _, e) -> "\\" ^  Variable.pp_var x ^ "." ^ pp_term e
   | Ap(e1, e2) -> "Ap(" ^ pp_term e1 ^ "," ^ pp_term e2 ^ ")"
   | Tup ts -> "<" ^ String.concat "," (List.map pp_term ts) ^ ">"
   | Split (e1, (vs, e2)) -> "split(" ^ pp_term e1 ^ " " ^ String.concat "." (List.map Variable.pp_var vs) ^ ". " ^ pp_term e2 ^ ")"
-  | Inj (t, i, e) -> "inj" ^ string_of_int i ^ " " ^ pp_term e
+  | Inj (_, i, e) -> "inj" ^ string_of_int i ^ " " ^ pp_term e
   | Case (t, arms) -> "case(" ^ pp_term t ^ ";" ^ String.concat ";" (List.map (fun (x, e) -> Variable.pp_var x ^ "." ^ pp_term e) arms)
   | Print (s, e) -> "print " ^ s ^"; " ^ pp_term e 
 
