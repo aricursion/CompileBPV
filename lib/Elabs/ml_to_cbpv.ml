63a82db61a2d2e98908564ee2be9d2ba9920ed3a
sonyasimk <sonya.simkin@gmail.com>
2024-04-17 05:23:44 -0400

    set up elab structure and edit cbpv_ast

--- lib/Elabs/ml_to_cbpv.ml
+++ lib/Elabs/ml_to_cbpv.ml
@@ -1,3 +1,14 @@
 open Cbpv_ast
 
-let elab (_ : Ast.term) : comp_term = failwith "Unimplemented"
+let rec elab (t : Ast.term) : comp_term = 
+  match t with
+  | Ast.Var x -> Ret (Var x)
+  | Lambda _ -> failwith "unimplemented"
+  | Ap _ -> failwith "unimplemented"
+  | Triv -> failwith "unimplemented"
+  | Tup _ -> failwith "unimplemented"
+  | Split _ -> failwith "unimplemented"
+  | Check _ -> failwith "unimplemented"
+  | Inj _ -> failwith "unimplemented"
+  | Case _ -> failwith "unimplemented"
+  | Print _ -> failwith "unimplemented"
