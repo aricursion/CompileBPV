baf6a04d9d1fb2d29ace79b17b211954ece8e32d
sonyasimk <sonya.simkin@gmail.com>
2024-04-17 05:24:42 -0400

    forgot to edit ml file

--- lib/ASTs/cbpv_ast.ml
+++ lib/ASTs/cbpv_ast.ml
@@ -2,16 +2,16 @@ type value_type =
 | Tensor of value_type * value_type
 | Sum of value_type * value_type
 | Unit
-| Susp of comp_type
+| U of comp_type
 
 and comp_type =
 | Times of value_type * value_type
 | Arr of value_type * comp_type
-
+| F of value_type
 
 type value_term =
 | Var of Variable.t
-| Tensor of value_term * value_term
+| TensorProd of value_term * value_term
 | Triv
 | Inj of value_type * int * value_term
 | Susp of comp_term
@@ -21,7 +21,7 @@ and comp_term =
 | Bind of comp_term * Variable.t * comp_term
 | Tuple of comp_term * comp_term
 | Proj of int * comp_term
-| Lam of Variable.t * comp_term
+| Lam of Variable.t * value_type * comp_term
 | Ap of comp_term * value_term
 | Force of value_term
 | Split of value_term * (Variable.t * Variable.t) * comp_term
